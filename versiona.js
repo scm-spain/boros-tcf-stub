f4dcc8ab7bf1ae520f0b11ea7768782855a3f755
Alex Castells <alextremp@hotmail.com>
2020-06-17 18:09:41 +0200

    versiona update

--- versiona.js
+++ versiona.js
@@ -1,17 +1,8 @@
 const versiona = require('versiona')
 
-const isMaster = () =>
-  process.env.TRAVIS_BRANCH === 'master' &&
-  process.env.TRAVIS_PULL_REQUEST === 'false'
-
-const publishCommand = () => {
-  if (process.env.TRAVIS_TAG.includes('-beta')) return false
-  const environment = isMaster() ? 'dev' : 'pro'
-  return `DEPLOY_ENV=${environment} npm run deploy`
-}
-
 versiona({
   repoOrg: 'scm-spain',
   repoName: 'boros-tcf-stub',
-  publish: publishCommand()
+  publish: false,
+  masterCommand: env => `DEPLOY_ENV=${env} npm run deploy`
 })
